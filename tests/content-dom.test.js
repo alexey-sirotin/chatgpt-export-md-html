c2f4d749dfa862eab7ca72c3e120835eb1721453
Morgana Hartwell <morgana.hartwell@gmail.com>
2026-09-26 01:58:33 +0300

    test: avoid jsdom vendor appearance assertion

--- tests/content-dom.test.js
+++ tests/content-dom.test.js
@@ -43,7 +43,6 @@ describe("content-script DOM selection behavior", () => {
 
     const firstBox = document.querySelector(".chatgpt-export-select");
     expect(firstBox.style.appearance).toBe("auto");
-    expect(firstBox.style.webkitAppearance).toBe("checkbox");
 
     document.querySelector("[data-user-message-bubble] .chatgpt-export-select").click();
     const selection = dispatch({ type: "GET_SELECTION" });
